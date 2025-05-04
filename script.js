a50d5c8f5867dc676ca4167fa7f3395d03e24500
2ndNM <roraldod567@gmail.com>
2025-05-05 03:04:51 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function showMore() {
   const moreInfo = document.getElementById("moreInfo");
   moreInfo.classList.toggle("hidden");
 }
+
+function openVideo() {
+  document.getElementById("videoOverlay").classList.remove("hidden");
+}
+
+function closeVideo() {
+  document.getElementById("videoOverlay").classList.add("hidden");
+}
